f71cad3498fe300a52dbf2f398cca9809ed90b61
lucasvinisan <lucasvinisan@zebra.local>
2022-10-04 15:36:01 -0300

    Questao 9 resolvida

--- exercices/9.sh
+++ exercices/9.sh
@@ -1,1 +1,17 @@
 #!bin/bash
+
+while
+do
+  echo "Digite uma palavra: "
+  read palavra
+
+  if[$palavra == "sair"]
+  then
+    break;
+  else
+    echo "$palavra" >>/tmp/listaOrdenada
+  fi
+done 
+
+echo "Lista"
+sort /tmp/listaOrdenada
